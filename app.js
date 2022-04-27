3860698ba25502a5e335a5f88a8e72fbbfc01b87
Hami Berkay AKTAŞ <hberkayaktas@gmail.com>
2022-04-27 23:35:02 +0300

    link doğrulama ve link yönlendirme yapıldı

--- app.js
+++ app.js
@@ -3,9 +3,24 @@ const express = require('express');
 const app = express();
 
 
+//teplate engine
+app.set("view engine","ejs");
+
+//middlewares;
+app.use(express.static("public"));
+
+
+//routes
 app.get('/', (req, res) => {
-      res.send('INDEX SAYFASI');
-})
+      res.status(200).render('index',{
+            page_name:"index"
+      })
+});
+app.get('/about', (req, res) => {
+      res.status(200).render('about',{
+            page_name:"about"
+      })
+});
 
 
 
