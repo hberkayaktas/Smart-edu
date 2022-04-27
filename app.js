b4605f42d591aff177b113d73a0db95ce3190170
Hami Berkay AKTAŞ <hberkayaktas@gmail.com>
2022-04-27 21:05:34 +0300

    çalışma ortamı oluşturuldu pcakage json

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+      res.send('INDEX SAYFASI');
+})
+
+
+
+const port = 3000;
+
+app.listen(port, () => {
+      console.log(`App started on port ${port}`);
+});
