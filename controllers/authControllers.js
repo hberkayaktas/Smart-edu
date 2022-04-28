34d88436b2dbd3f8cd6b3a0a8996ceadce05e30c
Hami Berkay AKTAŞ <hberkayaktas@gmail.com>
2022-04-29 01:53:59 +0300

    Bcrypt paketi ile giriş kontrolü yapıldı

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,16 +1,41 @@
-const User = require('../models/User');
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
 
     res.status(201).json({
-      status: 'success',
+      status: "success",
       user,
     });
   } catch (error) {
     res.status(400).json({
-      status: 'fail',
+      status: "fail",
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    
+    if (user) {
+      bcrypt.compare(password, user.password, (err, same) => {
+        if (same) {
+          // USER SESSION
+          res.json({
+            status: "success",
+            user,
+          });
+        }
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
       error,
     });
   }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,6 +4,7 @@ const authController = require("../controllers/authControllers");
 const router = express.Router();
 
 router.route("/signup").post(authController.createUser); // http://localhost:3000/categories
+router.route("/login").post(authController.loginUser); // http://localhost:3000/categories
 
 
 module.exports = router;
