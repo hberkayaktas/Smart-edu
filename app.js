98c67f107147db3de16a2050df75e848a31587b2
Hami Berkay AKTAŞ <hberkayaktas@gmail.com>
2022-04-28 18:58:56 +0300

    postman ile çalışıldı tüm kurslar sayfası oluşturuldu
    tekil kurslar sayfasından devam edilecek

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute =require('./routes/courseRoute')
 
 const app = express();
 
  //Connect DB
- mongoose.connect('mongodb://localhost/smartedu-db', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
-}).then(() => {
+ mongoose.connect('mongodb://localhost/smartedu-db')
+.then(() => {
       console.log('DB Connected Successfuly')
 });
 
@@ -19,7 +16,8 @@ app.set("view engine","ejs");
 
 //middlewares;
 app.use(express.static("public"));
-
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 //routes
 app.use('/',pageRoute);
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const courseController =require('../controllers/courseController');
+const express = require("express");
+const courseController = require("../controllers/courseControllers");
 
-const router = express. Router();
+const router = express.Router();
 
-router.route('/').post (courseController.createCourse); // http://localhost:3000/courses
+router.route("/").post(courseController.createCourse); // http://localhost:3000/courses
+router.route("/").get(courseController.getAllCourses);
+router.route("/:id").get(courseController.getCourse); // http://localhost:3000/courses/şajdfkafuıe6549 gibi bir link
 
 //örnek
-router.route('/yeni').post (courseController.createCourse); // http://localhost:3000/courses/yeni
+router.route("/yeni").post(courseController.createCourse); // http://localhost:3000/courses/yeni
 
 module.exports = router;
