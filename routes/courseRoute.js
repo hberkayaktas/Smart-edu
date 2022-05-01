72eb294144fbfb6bc79135463aee62baf41c6bc2
Hami Berkay AKTAŞ <hberkayaktas@gmail.com>
2022-05-01 04:16:52 +0300

    Kurs silme eventleri eklendi

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.route("/").post(roleMiddleware(["teacher","admin"]) ,courseController.createCourse); // http://localhost:3000/courses
 router.route("/").get(courseController.getAllCourses);
 router.route("/:slug").get(courseController.getCourse); // http://localhost:3000/courses/html-dersleri gibi bir link
+router.route("/:slug").delete(courseController.deleteCourse); // http://localhost:3000/courses/html-dersleri gibi bir link
 router.route("/enroll").post(courseController.enrollCourse);
 router.route("/release").post(courseController.releaseCourse);
 
